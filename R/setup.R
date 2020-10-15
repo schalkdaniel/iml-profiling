5920b17ac36dcdc40871e6e949d4db9391fbe43a
schalkdaniel <d-schalk@t-online.de>
2020-10-15 14:30:19 +0200

    initial commit

---
+++ R/setup.R
@@ -0,0 +1,10 @@
+ns         = c(100, 1000, 5000, 10000, 50000, 100000, 500000, 1000000)
+ps         = c(10, 20, 50, 100, 200, 500)
+batch_size = 10^seq_len(5L)
+learners   = c("regr.lm", "regr.randomForest", "regr.xgboost")
+rep        = seq_len(10L)
+
+experiments = expand.grid(n = ns, p = ps, learner = learners, batch = batch_size, rep = rep, stringsAsFactors = FALSE)
+experiments$hash = apply(X = experiments, MARGIN = 1L,  FUN = function (x) digest::sha1(x))
+
+
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# iml Benchmark
+
+### Structure:
+
+- `R` contains scripts etc.
+    - `R/profile-files` contains raw data resulting of profiling
+
+### Run on server:
+
+Clone the repository and run:
+```
+nohup Rscript run-profile.R > rlog.txt
+```
